ba5d306119be784edd22ad1db20269ccdc88de34
Benoît Caufriez <benoit.caufriez@gmail.com>
2014-01-28 19:27:20 +0100

    MaJ

--- src/client/model/Main.java
+++ src/client/model/Main.java
@@ -4,18 +4,6 @@ public class Main {
 	
 	public static void main(String[] args)
 	{
-		/*
-		RSA rsa_encryption = new RSA();
-		rsa_encryption.generateKeys();
-		
-		System.out.println("############### CLE PUBLIC ###############");
-		System.out.println("n = " + rsa_encryption.getPublic_key().getN());
-		System.out.println("e = " + rsa_encryption.getPublic_key().getE());
-		
-		System.out.println("############### CLE PRIVEE ###############");
-		System.out.println("n = " + rsa_encryption.getPrivate_key().getN());
-		System.out.println("u = " + rsa_encryption.getPrivate_key().getU());
-		*/
 		RSA alice = new RSA();
 		alice.generateKeys();
 		System.out.println("############### ALICE CLE PUBLIC ###############");
